67840d93be1f267c6d72373d593f94f8b12f230c
Seema1995 <seemapatilk4@gmail.com>
2022-02-11 11:23:26 +0530

    adding images folder and routing links

--- bank-app-UI/src/app/bank.service.ts
+++ bank-app-UI/src/app/bank.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankService {
 
-  constructor() { }
+baseUrl = 'http://localhost:8081/api'
+constructor(private _client:HttpClient) { }
+
+login(loginRequest : any) : Observable<any>{
+  let url = `${this.baseUrl}/login`
+  return this._client.post(url,loginRequest);
+}
+getAccountDetails(custId : any){
+  let url = `${this.baseUrl}/getDetails/${custId}`
+  return this._client.get(url);
+}
+
+getTransactionDetails(transactionRequestBody : any){
+  let url = `${this.baseUrl}/showAllTransactions`
+  return this._client.post(url,transactionRequestBody);
+}
+
+downloadExcel(transactionRequestBody :any){
+  let url = `${this.baseUrl}/customers/excel`
+  return this._client.post(url,transactionRequestBody);
+}
+
+changePassword(passwordRequestBody : any){
+  let url = `${this.baseUrl}/changePassword`
+  return this._client.put(url,passwordRequestBody);
+}
+
+performTransaction(transferRequestBody : any){
+  let url = `${this.baseUrl}/transfer`
+  return this._client.post(url,transferRequestBody);
+}
 }
