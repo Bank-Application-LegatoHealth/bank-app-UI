fc0b0bb2b9a00418e4f1887fbeb3fab8fd6ea172
shubham-tiwari95 <shubham283195@gmail.com>
2022-02-15 04:32:11 +0530

    Styled ChangePassword

--- bank-app-UI/src/app/change-password/change-password.component.js
+++ bank-app-UI/src/app/change-password/change-password.component.js
@@ -1,67 +1,20 @@
-var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirmPassword");
-
-document.getElementById('signupLogo').src = "assets/images/bank-logo.png";
-enableSubmitButton();
-
-function validatePassword() {
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-    return false;
-  } else {
-    confirm_password.setCustomValidity('');
-    return true;
+var check = function() {
+    if (document.getElementById('password').value ==
+      document.getElementById('confirmPassword').value) {
+      document.getElementById('message').style.fontFamily= 'Verdana, Geneva, Tahoma, sans-serif';
+      document.getElementById('message').style.fontSize="18px";
+      document.getElementById('message').style.display=flex;
+      document.getElementById('message').style.flexDirection=column;
+      document.getElementById('message').style.margin=auto;
+      document.getElementById('message').style.width="18px";
+      document.getElementById('message').innerHTML = 'Password Matched!';
+    } else {
+        document.getElementById('message').style.fontFamily= 'Verdana, Geneva, Tahoma, sans-serif';
+        document.getElementById('message').style.fontSize="18px";
+        document.getElementById('message').style.display=flex;
+        document.getElementById('message').style.flexDirection=column;
+        document.getElementById('message').style.margin=auto;
+        document.getElementById('message').style.width="18px";
+        document.getElementById('message').innerHTML = 'New Password and Confirm Password does not match';
+    }
   }
-}
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
-
-function enableSubmitButton() {
-  document.getElementById('submitButton').disabled = false;
-  document.getElementById('loader').style.display = 'none';
-}
-
-function disableSubmitButton() {
-  document.getElementById('submitButton').disabled = true;
-  document.getElementById('loader').style.display = 'unset';
-}
-
-function validateSignupForm() {
-  var form = document.getElementById('signupForm');
-  
-  for(var i=0; i < form.elements.length; i++){
-      if(form.elements[i].value === '' && form.elements[i].hasAttribute('required')){
-        console.log('There are some required fields!');
-        return false;
-      }
-    }
-  
-  if (!validatePassword()) {
-    return false;
-  }
-  
-  /*onSignup();*/
-}
-
-function onSignup() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    
-    disableSubmitButton();
-    
-    if (this.readyState == 4 && this.status == 200) {
-      enableSubmitButton();
-    }
-    else {
-      console.log('AJAX call failed!');
-      setTimeout(function(){
-        enableSubmitButton();
-      }, 1000);
-    }
-    
-  };
-  
-  xhttp.open("GET", "ajax_info.txt", true);
-  xhttp.send();
-}
